beef876dc68bcacf6d3b27b6708ec6a0ce1f0c04
Marc-François Cochaux-Laberge <dev@mfcl.io>
2018-12-21 13:18:37 -0500

    Implement basic layout with styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { hot } from 'react-hot-loader'
 
+import './styles/main.scss'
+
 import App from './App.jsx'
 
 ReactDOM.render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,26 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]_[local]_[hash:base64]',
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [htmlPlugin, hotModulePlugin],
